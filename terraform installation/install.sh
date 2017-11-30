e8724b598958d80dfbfdebf91b56540810c50129
Tejas Taunad H V <tejastaunad@gmail.com>
2017-11-30 11:34:18 +0530

    Update install.sh
    installing terraform 0.8 into /usr/bin

--- terraform installation/install.sh
+++ terraform installation/install.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-# this is to install terraform , configure its path and run it
-wget https://releases.hashicorp.com/terraform/0.8.8/terraform_0.8.8_linux_amd64.zip
-sudo apt-get install unzip
-sudo mkdir terraform
-sudo unzip terraform_0.8.8_linux_amd64.zip -d terraform
-touch pathfile
-echo 'export PATH=$PATH:your terraform path' >> pathfile
-cat pathfile >> .profile
-terraform --version
+sudo wget https://releases.hashicorp.com/terraform/0.8.8/terraform_0.8.8_linux_amd64.zip
+sudo unzip terraform_0.8.8_linux_amd64.zip -d /usr/bin
+echo 'export PATH=$PATH:/usr/bin/terraform' >> .profile
